4fbc53b4368cab706829458e7650baac3dc820a0
Lukas Nebesky <nebesluk@fit.cvut.cz>
2022-08-25 19:11:39 +0200

    Add project files.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace SDTDMM {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
